2bbea0294e8d9de919aeddc8f2785ff350831dd2
shinji.oda <shinji.oda@shiftsecurity.jp>
2022-06-08 10:37:49 +0900

    jest環境作成

--- next.config.js
+++ next.config.js
@@ -2,8 +2,12 @@ const webpack = require('webpack')
 const TsconfigPathsPlugin = require('tsconfig-paths-webpack-plugin')
 
 module.exports = {
+  swcMinify: true,
   webpack: (config, options) => {
     config.resolve.plugins = [new TsconfigPathsPlugin()]
     return config
+  },
+  compiler: {
+    reactRemoveProperties: true,
   },
 }
