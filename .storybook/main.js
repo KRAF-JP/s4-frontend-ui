6c739ad5406870b6087e41a7038978d7f1576733
shinji.oda <shinji.oda@shiftsecurity.jp>
2022-06-08 11:33:00 +0900

    storybook再導入

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,23 +1,15 @@
-const path = require('path')
-
 module.exports = {
-  stories: ['../src/**/*.stories.mdx', '../src/**/*.stories.@(js|jsx|ts|tsx)'],
-  addons: [
-    '@storybook/addon-links',
-    '@storybook/addon-essentials',
-    'storybook-addon-next-router',
+  "stories": [
+    "../src/**/*.stories.mdx",
+    "../src/**/*.stories.@(js|jsx|ts|tsx)"
   ],
-  webpackFinal: async (config) => {
-    config.resolve.alias = {
-      '@components': path.resolve(__dirname, '../src/components/'),
-      '@const': path.resolve(__dirname, '../src/const/'),
-      '@hooks': path.resolve(__dirname, '../src/hooks/'),
-      '@store': path.resolve(__dirname, '../src/store/'),
-      '@types': path.resolve(__dirname, '../src/types/'),
-    }
-    return config
-  },
-  core: {
-    builder: 'webpack5',
-  },
+  "addons": [
+    "@storybook/addon-links",
+    "@storybook/addon-essentials",
+    "@storybook/addon-interactions"
+  ],
+  "framework": "@storybook/react",
+  "core": {
+    "builder": "@storybook/builder-webpack5"
+  }
 }
