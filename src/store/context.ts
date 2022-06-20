3deb3b49f6fde7d8ab6c1258f161af589952e728
shinji.oda <shinji.oda@shiftsecurity.jp>
2022-06-20 13:05:37 +0900

    不要

--- src/store/context.ts
+++
@@ -1,7 +0,0 @@
-import { createContext, Dispatch } from 'react'
-import { userState, Action } from './interfaces'
-
-const GlobalContext =
-  createContext<{ state: userState; dispatch: Dispatch<Action> }>(null)
-
-export default GlobalContext
