1cd4bddd27b9ecdf1dd3f28ff81c7a0b815e88ec
shinji.oda <shinji.oda@shiftsecurity.jp>
2022-06-22 13:39:18 +0900

    data-testid付与

--- src/components/utils/global-head.tsx
+++ src/components/utils/global-head.tsx
@@ -9,9 +9,11 @@ const rootUrl = '/'
 
 const GlobalHead: React.FC<Props> = (props) => {
   return (
-    <Head>
+    <Head data-testid="utils-gh-head">
       {props.children}
-      <title>{props.title ? props.title + ' | S4' : 'S4'}</title>
+      <title data-testid="utils-gh-title">
+        {props.title ? props.title + ' | S4' : 'S4'}
+      </title>
       <link rel="icon" href={rootUrl + 'favicon.ico'} />
       <link
         href="https://fonts.googleapis.com/css2?family=Noto+Sans+JP:wght@100;300;400;500;700;900&display=swap"
